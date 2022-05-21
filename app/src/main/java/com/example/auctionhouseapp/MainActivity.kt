70d9f1bafa0aca46e2e63efe329e01606f238ee0
odaib <odai.badran@campus.technion.ac.il>
2022-05-22 00:29:01 +0300

    work: 15/5 :main activity layout and code + good design 16/5 : sign up activity and layout
    changing login and register design totally

--- app/src/main/java/com/example/auctionhouseapp/MainActivity.kt
+++ app/src/main/java/com/example/auctionhouseapp/MainActivity.kt
@@ -4,22 +4,27 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import com.example.auctionhouseapp.databinding.ActivityMainBinding
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FragmentNavigation{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btn_sign_up = findViewById<Button>(R.id.btn_sign_up)
-        val btn_log_in = findViewById<Button>(R.id.btn_log_in)
-        btn_sign_up.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.house,LoginFragment())
+            .commit()
+    }
+
+    override fun navigateFrag(fragment: Fragment, addToStack: Boolean) {
+        val transaction = supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.house,fragment)
+        if(addToStack){
+            transaction.addToBackStack(null)
         }
-        btn_log_in.setOnClickListener {
-            val intent = Intent(this, LogInActivity::class.java)
-            startActivity(intent)
-        }
+        transaction.commit()
     }
 }
