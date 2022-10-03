9ff8768d2ebc00f811e41f7659ab9087e67266ce
odaib <odai.badran@campus.technion.ac.il>
2022-10-03 22:25:12 +0300

    work: 15/5 :main activity layout and code + good design 16/5 : sign up activity and layout
    changing login and register design totally

--- app/src/main/java/com/example/auctionhouseapp/MainActivity.kt
+++ app/src/main/java/com/example/auctionhouseapp/MainActivity.kt
@@ -6,16 +6,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.auctionhouseapp.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class MainActivity : AppCompatActivity(), FragmentNavigation{
+    private lateinit var fAuth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fAuth = Firebase.auth
 
-        supportFragmentManager.beginTransaction()
-            .add(R.id.house,LoginFragment())
-            .commit()
+        val currenUser = fAuth.currentUser
+        if(currenUser != null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.house,AuctionHouseListFragment()).addToBackStack(null)
+                .commit()
+        } else {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.house,LoginFragment())
+                .commit()
+        }
+
+
     }
 
     override fun navigateFrag(fragment: Fragment, addToStack: Boolean) {
