87d2d6da8d2c14f812d4523d0809ac0f31cd270d
odaib <odai.badran@campus.technion.ac.il>
2022-10-12 23:25:15 +0300

    test checkUser function to have successful login

--- app/src/main/java/com/example/auctionhouseapp/LoginActivity.kt
+++ app/src/main/java/com/example/auctionhouseapp/LoginActivity.kt
@@ -185,7 +185,7 @@ class LoginActivity : AppCompatActivity() {
                             startActivity(intent)
                             finish()
                         } else {
-                            val intent = Intent(applicationContext, CustomerActivity::class.java) // TODO: UPDATE ACTIVITY
+                            val intent = Intent(applicationContext, HouseActivity::class.java) // TODO: UPDATE ACTIVITY
                             startActivity(intent)
                             finish()
                         }
