fd9d7e1502d23eb93797c2af835aa820a3573a8f
odaib <odai.badran@campus.technion.ac.il>
2022-10-19 22:12:05 +0300

    some changes in customer side not finihsed yet

--- app/src/main/java/com/example/auctionhouseapp/Activities/CustomerActivity.kt
+++ app/src/main/java/com/example/auctionhouseapp/Activities/CustomerActivity.kt
@@ -4,18 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.example.auctionhouseapp.Fragments.AuctionDaysSpinner
 import com.example.auctionhouseapp.Fragments.AuctionHousesListFragment
 import com.example.auctionhouseapp.Objects.Customer
 import com.example.auctionhouseapp.R
 import com.example.auctionhouseapp.Utils.FirebaseUtils
 
 class CustomerActivity : AppCompatActivity() {
-    val customer: Customer = Customer()
-    val list = AuctionHousesListFragment()
+    //val customer: Customer = Customer()
+    val List = AuctionHousesListFragment()
+    val LoadingFragment = AuctionDaysSpinner()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customer)
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentContainerView, LoadingFragment)
+            commit()
+        }
 
         findViewById<TextView>(R.id.txt_sign_out).setOnClickListener {
             FirebaseUtils.firebaseAuth.signOut()
@@ -23,16 +30,10 @@ class CustomerActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
-    }
 
-//    fun PerformAfterData() {
-//
-//        list.customer = customer
-//
-//        supportFragmentManager.beginTransaction().apply {
-//            replace(R.id.fragmentContainerView,list)
-//            commit()
-//        }
-//
-//    }
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentContainerView, List)
+            commit()
+        }
+    }
 }
